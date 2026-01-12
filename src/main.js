3232fe2ed9fc6e528ab06fc3d9747f41d38a57dd
NEEL <neelkamalpunj@gmail.com>
2026-01-12 23:50:10 +0530

    rendering started

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import"./style.css"
 import { getWeather } from "./weather";
 
-getWeather(30.744681, 76.665696, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-    data => {
-    console.log(data)
+getWeather(30.744681, 76.665696, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e => {
+    console.error(e)
+    alert("Error getting weather")
 })
+
+function renderWeather({current}){
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHouryWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+// function setValue(selector, value, {parent = document} = {}) {
+//     parent.querySelector(`[data-${selector}]`).textContent = value
+// }
+
+// function renderCurrentWeather(current) {
+//     setValue("current-temp", current.currentTemp)
+// }
+
+function renderCurrentWeather(current){
+    document.querySelector("[data-current-temp]").textContent = current.currentTemp
+}
